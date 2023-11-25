32f2acad4843315e7d12dc4cde56b55244cbbd70
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-11-25 15:10:22 +0530

    hero done

---
+++ src/pages/_app.tsx
@@ -0,0 +1,37 @@
+"use client";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import * as React from "react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { goerli, sepolia, avalancheFuji, polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [goerli, sepolia, avalancheFuji, polygonMumbai],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "ZexCraft",
+  projectId: process.env.NEXT_PROJECT_ID ?? "0354c8dc98c64f51775522050a4a0cfa",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
