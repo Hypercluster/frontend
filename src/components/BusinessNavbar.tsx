5738fd8c6cf75aed5e195acbae03cc424444a56e
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-11-27 20:48:29 +0530

    add home

--- src/components/BusinessNavbar.tsx
+++ src/components/BusinessNavbar.tsx
@@ -12,18 +12,7 @@ export default function BusinessNavbar({
   const [isNotification, setIsNotification] = useState(true);
   return (
     <div className="h-screen">
-      <div className="absolute right-0 h-[8%]">
-        <div className="h-full flex justify-between items-center">
-          <Link href={"/"} className="bg-[#171A33] rounded-full p-3 mr-8">
-            <FontAwesomeIcon
-              icon={faWallet}
-              className="text-lg text-gray-500"
-            />
-          </Link>
-        </div>
-      </div>
-
-      <div className="h-[100%]">{children}</div>
+      <div className="h-[100%] flex ">{children}</div>
     </div>
   );
 }
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -5,6 +5,7 @@ export default function CreatePage() {
   return (
     <BusinessNavbar>
       <Leftbar />
+      <div className="mx-16 mt-3 flex-1"></div>
     </BusinessNavbar>
   );
 }
